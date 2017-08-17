365527d52ddd5d3f9a5225603db6d822ae237d7c
ujjwal <ujjwalscience@protonmail.ch>
2017-08-17 02:26:07 +0200

    First draft of dataset base class

--- datasets/DatasetBase.py
+++ datasets/DatasetBase.py
@@ -1,6 +1,15 @@
-from abc import ABCMeta
+import abc
 
-
-class DatasetBase(metaclass=ABCMeta):
+class DatasetBase(abc.ABC):
     def __init__(self, name):
         self.name = name
+
+    @abc.abstractmethod
+    def annotationdictfor(self, imagefile):
+        pass
+
+    @abc.abstractmethod
+    def numclasses(self):
+        pass
+
+
