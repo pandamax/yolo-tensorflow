cb2934bc1d447e228a993b2c43998c51f1505bb0
ujjwal <ujjwalscience@protonmail.ch>
2017-08-24 02:07:44 +0200

    Started code for anchor priors

---
+++ yolo/anchorpriors.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import numpy as np
+from datasets import PascalVOC
+
+def getbboxes(basepath):
+    bboxes = []
+    dataset = PascalVOC.PascalVOC('PascalVOC2012', basepath)
+    for category in dataset.categorynames:
+        imagefiles = dataset.imagesfromcategory(category, subset="trainval")
+        for i in range(imagefiles.size):
+            annotationdict = dataset.annotationdictfor(imagefiles[i])
+            for key, value in annotationdict.items():
+                for v in value:
+                    bboxes+=v
+    bboxes = np.reshape(bboxes, (len(bboxes)/4, 4))
+    return bboxes
