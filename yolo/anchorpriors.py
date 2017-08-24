7d090b6169e96d59507f40cb49198f24cb3dd143
Ujjwal <ujjwal.ujjwal@inria.fr>
2017-08-24 15:12:56 +0200

    Modified to reflect changes to PascalVOC.py

--- yolo/anchorpriors.py
+++ yolo/anchorpriors.py
@@ -7,10 +7,11 @@ def getbboxes(basepath):
     dataset = PascalVOC.PascalVOC('PascalVOC2012', basepath)
     for category in dataset.categorynames:
         imagefiles = dataset.imagesfromcategory(category, subset="trainval")
-        for i in range(imagefiles.size):
+        for i in imagefiles.index.tolist():
             annotationdict = dataset.annotationdictfor(imagefiles[i])
             for key, value in annotationdict.items():
                 for v in value:
                     bboxes += v
+        print('Done for the category of {}'.format(category))
     bboxes = np.reshape(bboxes, (len(bboxes) / 4, 4))
     return bboxes
